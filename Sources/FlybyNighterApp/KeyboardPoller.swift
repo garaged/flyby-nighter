c5eaafb6ab401d8a2a34e5fd423320cce584d1aa
garaged <garaged@gmail.com>
2026-07-02 13:32:10 -0600

    Fix Space firing with hybrid key detection

--- Sources/FlybyNighterApp/KeyboardPoller.swift
+++ Sources/FlybyNighterApp/KeyboardPoller.swift
@@ -1,4 +1,5 @@
 #if os(macOS)
+import AppKit
 import CoreGraphics
 import Foundation
 import FlybyNighterCore
@@ -7,10 +8,25 @@ import FlybyNighterSpriteKit
 final class KeyboardPoller {
     private weak var scene: FlybyNighterScene?
     private var timer: Timer?
+    private var keyMonitor: Any?
     private var previousStartPressed = false
+    private var spacePressedByEvent = false
 
     init(scene: FlybyNighterScene) {
         self.scene = scene
+
+        keyMonitor = NSEvent.addLocalMonitorForEvents(matching: [.keyDown, .keyUp]) { [weak self] event in
+            guard event.keyCode == 49 else { return event }
+
+            if event.type == .keyDown {
+                self?.spacePressedByEvent = true
+            } else if event.type == .keyUp {
+                self?.spacePressedByEvent = false
+            }
+
+            return event
+        }
+
         timer = Timer.scheduledTimer(withTimeInterval: 1.0 / 60.0, repeats: true) { [weak self] _ in
             self?.poll()
         }
@@ -18,6 +34,9 @@ final class KeyboardPoller {
 
     deinit {
         timer?.invalidate()
+        if let keyMonitor {
+            NSEvent.removeMonitor(keyMonitor)
+        }
     }
 
     private func poll() {
@@ -32,7 +51,7 @@ final class KeyboardPoller {
         if pressed(13) || pressed(126) { y += 1 }
 
         scene.setMovement(Vector2(x: x, y: y))
-        scene.setFiring(pressed(49))
+        scene.setFiring(spacePressedByEvent || pressed(49))
 
         let startPressed = pressed(36) || pressed(76)
         if startPressed && !previousStartPressed {
@@ -42,7 +61,8 @@ final class KeyboardPoller {
     }
 
     private func pressed(_ keyCode: CGKeyCode) -> Bool {
-        CGEventSource.keyState(.combinedSessionState, key: keyCode)
+        CGEventSource.keyState(.combinedSessionState, key: keyCode) ||
+        CGEventSource.keyState(.hidSystemState, key: keyCode)
     }
 }
 #endif
